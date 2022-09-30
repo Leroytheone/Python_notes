b4e3c6f8f851f4163c5bcfa51f7d00e9a2964d23
Leroytheone <leroy673412@gmail.com>
2022-09-30 11:57:04 +0200

    437856

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
-'''
-def reverseWords(input):
+'''def reverseWords(input):
      
     # 通过空格将字符串分隔符，把各个单词分隔为列表
     inputWords = input.split(" ")
@@ -21,9 +20,7 @@ def reverseWords(input):
 if __name__ == "__main__":
     input = 'I like runoob'
     rw = reverseWords(input)
-    print(rw)
-    
-'''
+    print(rw)'''
 
 #var1 = "Hello world"
 #var1[-5:-1]
@@ -40,7 +37,7 @@ def printme( str ):
 # 调用 printme 函数，不加参数会报错
 printme('2sfd')'''
 
-# 可写函数说明
+'''# 可写函数说明
 def sum( arg1, arg2 ):
    # 返回2个参数的和."
    total = arg1 + arg2
@@ -50,4 +47,141 @@ def sum( arg1, arg2 ):
 # 调用sum函数
 a = sum( 10, 20 )
 #print ("函数外 : ", tota)
-type(a)
+type(a)'''
+
+'''import os
+import time
+
+
+def main():
+    content = '北京欢迎你为你开天辟地......'
+    while True:
+        # 清理屏幕上的输出
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
+
+
+if __name__ == '__main__':
+    main()'''
+    
+'''class Student(object):
+
+    # __init__是一个特殊方法用于在创建对象时进行初始化操作
+    # 通过这个方法我们可以为学生对象绑定name和age两个属性
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def study(self, course_name):
+        print('%s正在学习%s.' % (self.name, course_name))
+
+    # PEP 8要求标识符的名字用全小写多个单词用下划线连接
+    # 但是部分程序员和公司更倾向于使用驼峰命名法(驼峰标识)
+    def watch_movie(self):
+        if self.age < 18:
+            print('%s只能观看《熊出没》.' % self.name)
+        else:
+            print('%s正在观看岛国爱情大电影.' % self.name)
+            
+def main():
+    # 创建学生对象并指定姓名和年龄
+    stu1 = Student('骆昊', 38)
+    # 给对象发study消息
+    stu1.study('Python程序设计')
+    # 给对象发watch_av消息
+    stu1.watch_movie()
+    stu2 = Student('王大锤', 15)
+    stu2.study('思想品德')
+    stu2.watch_movie()
+
+
+if __name__ == '__main__':
+    main()'''
+    
+'''class Test:
+
+    def __init__(self, foo):
+        self.foo = foo
+
+    def bar(self):
+        print(self.foo)
+        print('__bar')
+
+
+def main():
+    test = Test('hello')
+    # AttributeError: 'Test' object has no attribute '__bar'
+    test.bar()
+    # AttributeError: 'Test' object has no attribute '__foo'
+    print(test.foo)
+
+
+if __name__ == "__main__":
+    main()'''
+    
+'''class Test:
+
+    def __init__(self, foo):
+        self.__foo = foo
+
+    def __bar(self):
+        print(self.__foo)
+        print('__bar')
+
+
+def main():
+    test = Test('hello')
+    test._Test__bar()
+    print(test._Test__foo)
+
+
+if __name__ == "__main__":
+    main()'''
+    
+from time import sleep
+
+
+class Clock(object):
+    """数字时钟"""
+
+    def __init__(self, hour=0, minute=0, second=0):
+        """初始化方法
+
+        :param hour: 时
+        :param minute: 分
+        :param second: 秒
+        """
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+
+    def run(self):
+        """走字"""
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def show(self):
+        """显示时间"""
+        return '%02d:%02d:%02d' % \
+               (self._hour, self._minute, self._second)
+
+
+def main():
+    clock = Clock(23, 59, 58)
+    while True:
+        print(clock.show())
+        sleep(1)
+        clock.run()
+
+
+if __name__ == '__main__':
+    main()
